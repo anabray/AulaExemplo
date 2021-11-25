a6aea6f72669bfa3e0bedb404560013412d6cb7c
ANA MARIA FUCCILLE <ana.mfuccille@senacsp.edu.br>
2021-11-25 17:42:03 -0200

    update

---
+++ MovePlayerToMouseClickLocation_TPJD2/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animator anim;
+    private AudioSource audio;
+
+    public string parametroAnim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            anim.SetBool(parametroAnim, true);
+            if (!audio.isPlaying) audio.Play();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            anim.SetBool(parametroAnim, false);
+            audio.Stop();
+        }
+    }
+}
--- MovePlayerToMouseClickLocation_TPJD2/Assets/Scripts/SnakeController.cs
+++ MovePlayerToMouseClickLocation_TPJD2/Assets/Scripts/SnakeController.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class SnakeController : MonoBehaviour
 {
     private Animator anim;
+    private AudioSource audio;
     
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
+        {
             anim.SetBool("Shaking", true);
+            if (!audio.isPlaying) audio.Play();
+        }
     }
 
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
+        {
             anim.SetBool("Shaking", false);
+            audio.Stop();
+        }
     }
 }
